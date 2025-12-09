02c541e37cb7eb380a2bc2f71775dcdd1c3d18aa
Zoltan Olasz <zoltan.olasz.tech@gamil.com>
2025-12-09 11:20:16 +1000

    Initial commit

---
+++ Project-01/terraform-multi-tier-app/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "example" {
+  ami = "ami-007e5a061b93ceb2f"
+  instance_type = "t3.micro"
+
+user_data = <<-EOF
+            #!/bin/bash
+            yum install -y httpd
+            systemctl start httpd
+            systemctl enable httpd
+            echo '<h1>Hello from Terraform!</h1>' > /var/www/html/index.html
+            EOF
+
+  tags = {
+    Name = "SimpleWebServer"
+  }
+}
